595305071a28bae7834e6e0c8c86faaf09c0f2cb
CoRock <mikecorock@gmail.com>
2018-04-26 21:43:51 +0900

    $ join example

---
+++ lectures/src/com/corock/day07/JoinEx.java
@@ -0,0 +1,29 @@
+package com.corock.day07;
+
+class MyRunnableTwo implements Runnable {
+	public void run() 		{ System.out.println("run");	first(); }
+	public void first() 	{ System.out.println("first");	second(); }
+	public void second() 	{ System.out.println("second"); }
+}
+
+public class JoinEx {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println(Thread.currentThread().getName() + " start");
+		
+		Runnable r = new MyRunnableTwo();
+		Thread myThread = new Thread(r);
+		myThread.start();
+		
+		myThread.join();
+		
+/*
+ * I'll do exception handling in person!
+ * 	=> try-catch
+
+	try { myThread.join(); }
+	catch(InterruptedException ie) { ie.printStackTrace(); }
+*/
+		
+		System.out.println(Thread.currentThread().getName() + " end");
+	}
+}
